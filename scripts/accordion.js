3a96250307795b9be39839b13fe9af98b0c44377
Philipp Gerstung <mail@philippgerstung.de>
2020-03-11 19:15:52 +0100

    Added default open attribute to accordion

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -3,14 +3,19 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    toggleAccordion(this);
   });
+  if(acc[i].dataset.default === "open") toggleAccordion(acc[i]);
+}
+
+function toggleAccordion(elmnt){
+  elmnt.classList.toggle("active");
+  var panel = elmnt.nextElementSibling;
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  } 
 }
 
 // Scroll to Top when clicking on the top left name
