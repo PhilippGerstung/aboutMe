af30aa9e00406e9b0777744c997c1c9322a70dea
Philipp Gerstung <mail@philippgerstung.de>
2020-03-13 14:04:39 +0100

    Fix in accordion automatic height

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,12 +1,4 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    toggleAccordion(this);
-  });
-  if(acc[i].dataset.default === "open") toggleAccordion(acc[i]);
-}
 
 function toggleAccordion(elmnt){
   elmnt.classList.toggle("active");
@@ -14,12 +6,27 @@ function toggleAccordion(elmnt){
   if (panel.style.maxHeight) {
     panel.style.maxHeight = null;
   } else {
-    panel.style.maxHeight = panel.scrollHeight + "px";
+    panel.style.maxHeight = "fit-content";
+    // panel.style.maxHeight = panel.scrollHeight + "px";
   } 
 }
 
-// Scroll to Top when clicking on the top left name
-document.getElementById("header-area-name").addEventListener("click", function(){
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      toggleAccordion(this);
+    });
+    if(acc[i].dataset.default === "open") toggleAccordion(acc[i]);
+  }
+
+  // Scroll to Top when clicking on the top left name
+  document.getElementById("header-area-name").addEventListener("click", function(){
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  });
+}, false);
